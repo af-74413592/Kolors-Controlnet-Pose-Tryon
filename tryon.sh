ee80c0fad2abf5700000d3f1cae5beebc06b46f5
af-74413592 <74224119+af-74413592@users.noreply.github.com>
2025-01-28 00:23:43 +0800

    Update tryon.sh

--- tryon.sh
+++ tryon.sh
@@ -1,1 +1,1 @@
-python controlnet/sample_controlNet_ipadapter_pose_tryon.py
+python controlnet/sample_controlNet_ipadapter_pose_tryon.py ./00000_00.jpg ./00001_00.jpg 图片上的模特穿着白色T恤上面有着红色Levis字块，搭配银色的耳环。 Pose
